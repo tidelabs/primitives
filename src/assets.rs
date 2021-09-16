6baa4b82bf400d59f9f725e9378b6c1100b04c7b
David Lemarier <david@lemarier.ca>
2021-09-16 07:10:47 -0400

    Initial commit

---
+++ src/assets.rs
@@ -0,0 +1,15 @@
+use codec::{Decode, Encode};
+#[cfg(feature = "std")]
+use serde::{Deserialize, Serialize};
+use sp_core::RuntimeDebug;
+
+#[derive(Encode, Decode, Copy, Clone, PartialEq, Eq, Ord, PartialOrd, RuntimeDebug)]
+#[cfg_attr(feature = "std", derive(Serialize, Deserialize))]
+pub enum AssetId {
+  /// TIDE native currency of the chain
+  TIDE,
+  /// Generic enumerated asset
+  /// Range 0 - 0x00000000FFFFFFFF (2^32)-1 is reserved for protected tokens
+  /// the values under 1000 are used for ISO 4217 Numeric Curency codes
+  Asset(u64),
+}
