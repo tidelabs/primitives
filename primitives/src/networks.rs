9ce4fc185781480ef7e63c9054ce87f32c8de932
Evan Feenstra <evanfeenstra@gmail.com>
2023-11-14 09:30:08 -0800

    ath local addy

--- primitives/src/networks.rs
+++ primitives/src/networks.rs
@@ -121,6 +121,11 @@ impl Asset {
         ("testnet", "0xbce2733e4b0eb15278ebb9f8496d3c638d1f43dd"),
         ("mainnet", "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48"),
       ]));
+    } else if self == &Asset::AllTimeHigh {
+      return Some(str_map(vec![(
+        "local",
+        "0x8a791620dd6260079bf849dc5567adc3f2fdc318",
+      )]));
     }
     None
   }
